5c8cd4650edddcfbdb11137c978bf8e10b2fdf55
tudor <pagu.tudor@gmail.com>
2025-11-19 15:08:26 +0100

    add pebs and format

--- linux/mm/thp_pebs_shrinker.c
+++ linux/mm/thp_pebs_shrinker.c
@@ -186,14 +186,13 @@
 // 	return result;
 // }
 
-
 struct utilization_bit_vector {
 	DECLARE_BITMAP(bitmap, 512);
 };
 
 struct pmd_to_split_deferred_node {
 	unsigned long addr;
-	struct mm_struct* mm;
+	struct mm_struct *mm;
 	struct llist_node node;
 };
 
@@ -204,20 +203,23 @@ enum poll_result {
 	BACK_OFF,
 };
 
-static int poll_split_list_once(void) {
+static int poll_split_list_once(void)
+{
 	if (llist_empty(&pmd_to_split_deferred_list)) {
 		return BACK_OFF; // back off
 	}
 
-	struct llist_node* first = llist_del_first(&pmd_to_split_deferred_list);
-	struct pmd_to_split_deferred_node* first_node = container_of(first, struct pmd_to_split_deferred_node, node);
+	struct llist_node *first = llist_del_first(&pmd_to_split_deferred_list);
+	struct pmd_to_split_deferred_node *first_node =
+		container_of(first, struct pmd_to_split_deferred_node, node);
 	thp_collapse_anonymous_pmd(first_node->mm, first_node->addr);
 	kfree(first_node);
 	return SUCCESS;
 }
 
-static int thp_collapser_thread(void *data) {
-	while(!kthread_should_stop()) {
+static int thp_collapser_thread(void *data)
+{
+	while (!kthread_should_stop()) {
 		int result = poll_split_list_once();
 		if (result == BACK_OFF) {
 			msleep(10);
@@ -228,29 +230,32 @@ static int thp_collapser_thread(void *data) {
 
 // we should not be in atomic context here... but we probably are holding
 // mmap_lock
-int record_page_fault_for_thp_shrinking(struct mm_struct* mm, unsigned long addr, int nr_pages) {
+int record_page_fault_for_thp_shrinking(struct mm_struct *mm,
+					unsigned long addr, int nr_pages)
+{
 	// I'm assuming this can't ever happen. If it does happen,
 	// I want to know about it
 	BUG_ON(nr_pages > 512);
-	
+
 	u64 pmd_page_number = (addr >> PMD_SHIFT);
-	void* entry = xa_load(mm->thp_usage, pmd_page_number);
-	struct utilization_bit_vector* bit_vector;
+	void *entry = xa_load(mm->thp_usage, pmd_page_number);
+	struct utilization_bit_vector *bit_vector;
 	if (entry == NULL) {
 		bit_vector = kmalloc(sizeof(*bit_vector), GFP_KERNEL);
 		if (!bit_vector) {
 			return -ENOMEM;
 		}
-		xa_store(mm->thp_usage, pmd_page_number, bit_vector, GFP_KERNEL);
+		xa_store(mm->thp_usage, pmd_page_number, bit_vector,
+			 GFP_KERNEL);
 	} else {
-		bit_vector = (struct utilization_bit_vector*)entry;
+		bit_vector = (struct utilization_bit_vector *)entry;
 	}
 
 	// address is like:
 	// | ... | 9 bits of offset within PTE | 12 bits of offset within Page
-	// this shuold be the index of the page we are within the PTE page, 
+	// this shuold be the index of the page we are within the PTE page,
 	// so from 0 to 511 inclusive.
-	int base_page_within_pmd = ((addr & ((1 << 21) - 1) ) >> 12);
+	int base_page_within_pmd = ((addr & ((1 << 21) - 1)) >> 12);
 	BUG_ON(base_page_within_pmd < 0 || base_page_within_pmd >= 512);
 
 	// set the appropiate bits atomically!
@@ -261,7 +266,8 @@ int record_page_fault_for_thp_shrinking(struct mm_struct* mm, unsigned long addr
 	// very simple criterion
 	int weight = bitmap_weight(bit_vector->bitmap, 512);
 	if (weight >= 256) {
-		struct pmd_to_split_deferred_node* node = kmalloc(sizeof(*node), GFP_KERNEL);
+		struct pmd_to_split_deferred_node *node =
+			kmalloc(sizeof(*node), GFP_KERNEL);
 		if (!node) {
 			return -ENOMEM;
 		}
@@ -273,8 +279,7 @@ int record_page_fault_for_thp_shrinking(struct mm_struct* mm, unsigned long addr
 	return 0;
 }
 
-
-struct task_struct* collapser_thread;
+struct task_struct *collapser_thread;
 static DEFINE_MUTEX(collapser_mutex);
 
 SYSCALL_DEFINE0(enable_thp_pebs_shrinking)
@@ -290,7 +295,8 @@ SYSCALL_DEFINE0(enable_thp_pebs_shrinking)
 		ret = -1;
 	} else {
 		thread_started = true;
-		collapser_thread = kthread_run(thp_collapser_thread, NULL, "thp_collapser_thread");
+		collapser_thread = kthread_run(thp_collapser_thread, NULL,
+					       "thp_collapser_thread");
 		ret = 0;
 	}
 
@@ -298,7 +304,57 @@ SYSCALL_DEFINE0(enable_thp_pebs_shrinking)
 	return ret;
 }
 
+// --- PEBS START ----
+#define ALL_STORES 0x82d0
+static struct perf_event_attr wd_hw_attr = {
+	.type = PERF_TYPE_SOFTWARE,
+	.config = PERF_COUNT_SW_CPU_CLOCK,
+	.size = sizeof(struct perf_event_attr),
+	.pinned = 1,
+	.disabled = 1,
+};
+
+static void watchdog_overflow_callback(struct perf_event *event,
+				       struct perf_sample_data *data,
+				       struct pt_regs *regs)
+{
+	printk("got called from pebs!\n");
+}
+
+static void register_pebs(void)
+{
+	static bool registered = false;
+	if (registered) {
+		printk("tpagu debug: skipping registering pebs event, already registered...\n");
+		return;
+	}
+
+	unsigned int cpu;
+	struct perf_event_attr *wd_attr;
+	struct perf_event *evt;
+
+	cpu = raw_smp_processor_id();
+	wd_attr = &wd_hw_attr;
+	wd_attr->sample_period = 1000;
+	evt = perf_event_create_kernel_counter(
+		wd_attr, cpu, NULL, watchdog_overflow_callback, NULL);
+	if (IS_ERR(evt)) {
+		printk("failed to create event...\n");
+		long err = PTR_ERR(evt);
+		pr_err("perf: create failed cpu=%u err=%ld\n", cpu, err);
+		return;
+	} else {
+		registered = true;
+		printk("created event!\n");
+	}
+}
+
+// --- PEBS END ---
+
 // ----- SYSFS_START -----
+static void enable_pebs_shrinker(void) {
+	register_pebs();
+}
 
 enum thp_pebs_shrinker_flag {
 	PEBS_SHRINKER_ENABLED,
@@ -306,8 +362,10 @@ enum thp_pebs_shrinker_flag {
 
 static unsigned long thp_pebs_shrinker_flags = 0;
 
-static ssize_t pebs_enabled_show(struct kobject* kobj, struct kobj_attribute* attr, char *buf) {
-	const char* output;
+static ssize_t pebs_enabled_show(struct kobject *kobj,
+				 struct kobj_attribute *attr, char *buf)
+{
+	const char *output;
 	if (test_bit(PEBS_SHRINKER_ENABLED, &thp_pebs_shrinker_flags)) {
 		output = "[enable] disable";
 	} else {
@@ -315,19 +373,25 @@ static ssize_t pebs_enabled_show(struct kobject* kobj, struct kobj_attribute* at
 	}
 	return sysfs_emit(buf, "%s\n", output);
 }
-static ssize_t pebs_enabled_store(struct kobject *kobj, struct kobj_attribute *attr, const char* buf, size_t count) {
+static ssize_t pebs_enabled_store(struct kobject *kobj,
+				  struct kobj_attribute *attr, const char *buf,
+				  size_t count)
+{
 	printk("in store\n");
 	ssize_t ret = count;
-	if (sysfs_streq(buf, "enable")) 
+	if (sysfs_streq(buf, "enable")) {
+		enable_pebs_shrinker();
 		set_bit(PEBS_SHRINKER_ENABLED, &thp_pebs_shrinker_flags);
+	}
 	else if (sysfs_streq(buf, "disable"))
 		clear_bit(PEBS_SHRINKER_ENABLED, &thp_pebs_shrinker_flags);
-	else 
+	else
 		ret = -EINVAL;
 
 	return ret;
 }
-static struct kobj_attribute pebs_shrinker_enabled_attr = __ATTR_RW(pebs_enabled);
+static struct kobj_attribute pebs_shrinker_enabled_attr =
+	__ATTR_RW(pebs_enabled);
 static struct attribute *pebs_shrinker_attr[] = {
 	&pebs_shrinker_enabled_attr.attr,
 	NULL,
@@ -336,16 +400,19 @@ static const struct attribute_group pebs_shrinker_attr_group = {
 	.attrs = pebs_shrinker_attr
 };
 
-static int __init thp_pebs_shrinker_init(void) {
+static int __init thp_pebs_shrinker_init(void)
+{
 	int err;
 	struct kobject *thp_pebs_shrinker_kobj;
-	thp_pebs_shrinker_kobj = kobject_create_and_add("thp_pebs_shrinker", mm_kobj);
-	if (unlikely(!thp_pebs_shrinker_kobj )) {
+	thp_pebs_shrinker_kobj =
+		kobject_create_and_add("thp_pebs_shrinker", mm_kobj);
+	if (unlikely(!thp_pebs_shrinker_kobj)) {
 		pr_err("failed to create thp pebs shrinker kobject\n");
 		return -ENOMEM;
 	}
 
-	err = sysfs_create_group(thp_pebs_shrinker_kobj, &pebs_shrinker_attr_group);
+	err = sysfs_create_group(thp_pebs_shrinker_kobj,
+				 &pebs_shrinker_attr_group);
 	if (err) {
 		pr_err("failed to register thp pebs shrinker group\n");
 		goto delete_obj;
@@ -358,8 +425,6 @@ delete_obj:
 	return err;
 }
 
-
 subsys_initcall(thp_pebs_shrinker_init);
-
 
 // --- SYSFS END -----
