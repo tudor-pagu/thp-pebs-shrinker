bfe3d2dcd3968d558e25b7022b6edc75111c277e
Makar <kulmak41@gmail.com>
2025-12-14 00:00:20 +0100

    remove technical files

--- pebs_test_module/.pebs_test.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_pebs_test.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /home/advos1/thp-pebs-shrinker/linux/scripts/module.lds -o pebs_test.ko pebs_test.o pebs_test.mod.o .module-common.o
